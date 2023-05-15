d418a5a0c3a06ad97e9291aed64ad9de7160e1f2
artemovit <brunewitch@mail.ru>
2023-05-15 13:55:24 +0300

    added repertuar controller

--- server/controllers/repertuarController.js
+++ server/controllers/repertuarController.js
@@ -1,9 +1,27 @@
+const {Repertuar} = require('../models/models')
+const ApiError = require("../error/ApiError")
+const uuid = require('uuid')
+const path = require('path')
+
 class RepertuarController {
-    async create(req, res){
+    async create(req, res, next){
+        try{
+            const {name, author, director, discription, time, children, host} = req.body
+            const {mainPhoto} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            mainPhoto.mv(path.resolve(__dirname, '..', 'static', fileName))
+            const repertuar = await Repertuar.create({name, author, director, discription, time, children, host, mainPhoto: fileName})
+            return res.json(repertuar)
+        }
+        catch(e){
+            next(ApiError.badRequest(e.message))
+        }
 
     }
     async get(req, res){
-        
+        let repertuar;
+        repertuar = await Repertuar.findAll()
+        return res.json(repertuar)
     }
     async getOne(req, res){
         
